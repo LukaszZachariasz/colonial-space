c900970c527e431f39ae15313e57393bfe7571c3
tomasz.pasternak <pstomek@gmail.com>
2022-03-07 09:20:11 +0100

    example of skybox image

--- src/game-objects/skybox/space-skybox/space-skybox.ts
+++ src/game-objects/skybox/space-skybox/space-skybox.ts
@@ -9,7 +9,11 @@ export class SpaceSkybox implements Skybox {
         this.skybox = BABYLON.MeshBuilder.CreateBox('spaceSkyBox', {size: 1000.0}, scene);
         this.material = new BABYLON.StandardMaterial('spaceSkyBox', scene);
         this.material.backFaceCulling = false;
-        this.material.reflectionTexture = new BABYLON.CubeTexture('resources/skybox/space-skybox/', scene);
+        this.material.reflectionTexture = new BABYLON.CubeTexture(
+            'resources/skybox/space-skybox/',
+            scene,
+            ['_px.png', '_py.png', '_pz.png', '_nx.png', '_ny.png', '_nz.png']
+        );
         this.material.reflectionTexture.coordinatesMode = BABYLON.Texture.SKYBOX_MODE;
         this.material.diffuseColor = new BABYLON.Color3(0, 0, 0);
         this.material.specularColor = new BABYLON.Color3(0, 0, 0);
