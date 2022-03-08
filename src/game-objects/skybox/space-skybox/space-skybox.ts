8b6793913ec1b1ee85d099c99d9f296c85e7bb78
lukasz.zachariasz <lukaszzachariasz.contactme@gmail.com>
2022-03-08 01:17:15 +0100

    Added galaxy init

--- src/game-objects/skybox/space-skybox/space-skybox.ts
+++ src/game-objects/skybox/space-skybox/space-skybox.ts
@@ -1,5 +1,7 @@
 import * as BABYLON from 'babylonjs';
+import {AbstractMesh} from 'babylonjs/Meshes/abstractMesh';
 import {Skybox} from '../skybox';
+import {Vector3} from 'babylonjs';
 
 export class SpaceSkybox implements Skybox {
     public skybox: BABYLON.Mesh;
@@ -15,5 +17,24 @@ export class SpaceSkybox implements Skybox {
         this.material.specularColor = new BABYLON.Color3(0, 0, 0);
         this.material.alphaCutOff = 0.5;
         this.skybox.material = this.material;
+
+        BABYLON.SceneLoader.Append('resources/galaxies/', 'galaxy.glb', scene);
+        BABYLON.SceneLoader.Append('resources/galaxies/', 'galaxy.glb', scene);
+
+        scene.executeWhenReady(() => {
+            const galaxies: AbstractMesh[] = scene.meshes.filter((el: AbstractMesh) => el.name.includes('galaxy'));
+
+            galaxies.forEach((galaxy: AbstractMesh, index: number) => {
+                galaxy.position = new Vector3(300 + (index * 100), index * 100, 300 + (index * 100));
+                galaxy.rotation = new Vector3(100, 20, 50);
+                galaxy.scaling = new Vector3(100, 100, 100);
+            });
+
+            galaxies.forEach((galaxy: AbstractMesh) => {
+                galaxy.rotate(new Vector3(0, 0, -1), 0.0001);
+                galaxy.rotate(new Vector3(-1, -1, -1), 0.0001);
+            });
+        });
+    
     }
 }
