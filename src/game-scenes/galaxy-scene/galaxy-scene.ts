d1fe57b36b01a064f7674a1fb076d4cf825f29f1
tomasz.pasternak <pstomek@gmail.com>
2022-03-09 15:03:37 +0100

    fix sort imports

--- src/game-scenes/galaxy-scene/galaxy-scene.ts
+++ src/game-scenes/galaxy-scene/galaxy-scene.ts
@@ -2,9 +2,9 @@ import * as BABYLON from 'babylonjs';
 import {GalaxyArea} from '../../game-objects/galaxy-area/galaxy-area';
 import {GameSceneGui} from '../game-scene-gui';
 import {GameSceneLoading} from '../game-scene-loading';
-import {SpaceSkybox} from '../../game-objects/skybox/space-skybox/space-skybox';
-import {GeneratedGalaxyOrigin} from '../../game-objects/galaxy/generated-galaxy-origin';
 import {GeneratedGalaxyDust} from '../../game-objects/galaxy/generated-galaxy-dust';
+import {GeneratedGalaxyOrigin} from '../../game-objects/galaxy/generated-galaxy-origin';
+import {SpaceSkybox} from '../../game-objects/skybox/space-skybox/space-skybox';
 
 
 export class GalaxyScene extends GameSceneLoading {
@@ -13,6 +13,6 @@ export class GalaxyScene extends GameSceneLoading {
     public skybox: SpaceSkybox;
     public galaxyAreas: GalaxyArea[] = [];
     public gui: GameSceneGui;
-    public generatedGalaxyOrigin: GeneratedGalaxyOrigin
+    public generatedGalaxyOrigin: GeneratedGalaxyOrigin;
     public generatedGalaxyDust: GeneratedGalaxyDust;
 }
