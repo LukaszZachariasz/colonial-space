bfa126d93e427ed63ae502f1554ca2b95e865323
tomasz.pasternak <tomasz.pasternak@silevis.com>
2022-03-13 10:14:05 +0100

    some code refactoring

--- src/game-core/game-loader/game-loader.ts
+++ src/game-core/game-loader/game-loader.ts
@@ -1,9 +1,9 @@
-import {gamePlatform} from '../../core/game-platform';
 import {GameScene} from '../../game-scenes/game-scene';
-import {LoadingScene} from '../../game-scenes/loading-scene/loading-scene';
 import {GameplayState} from '../game-state/gameplay-state/gameplay-state';
 import {LoadGalaxyScene} from './load-galaxy-scene/load-galaxy-scene';
+import {LoadingScene} from '../../game-scenes/loading-scene/loading-scene';
 import {filter, take, tap} from 'rxjs';
+import {gamePlatform} from '../../core/game-platform';
 import gameState from '../game-state/game-state';
 import sceneLoader from '../../engine/scene-loader/scene-loader';
 
