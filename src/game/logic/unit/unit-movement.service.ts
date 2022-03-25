bed55869cfe13a134cf901ed27ef319873a4a92c
tomasz.pasternak <tomasz.pasternak@silevis.com>
2022-03-25 13:34:56 +0100

    remove console logs

--- src/game/logic/unit/unit-movement.service.ts
+++ src/game/logic/unit/unit-movement.service.ts
@@ -3,12 +3,9 @@ import {UnitState} from '../../store/unit/unit.state';
 import {logic} from '../../game';
 
 export class UnitMovementService {
-
     public planMovingForUnit(unit: UnitState, destinationSquareId: string): void {
         const destination: BABYLON.Vector2 = logic().squareService.getSquarePosition(destinationSquareId);
         let currentDimensions: BABYLON.Vector2 = logic().squareService.getSquarePosition(unit.squareId);
-
-        console.log(destination.x, destination.y);
 
         unit.plannedMovement = [];
 
@@ -21,9 +18,7 @@ export class UnitMovementService {
                     destination.y > currentDimensions.y ? currentDimensions.y + 1 : currentDimensions.y - 1
             );
 
-            console.log(currentDimensions.x, currentDimensions.y);
             unit.plannedMovement.push(logic().squareService.getSquareByPosition(currentDimensions).id);
         }
-        console.log(unit.plannedMovement);
     }
 }
