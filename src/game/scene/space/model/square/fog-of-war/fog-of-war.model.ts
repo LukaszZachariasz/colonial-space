f8e86a07ce4518f566eeaf8fb0a0399645b09e29
Zachariasz, Lukasz (ext.) <lukasz.zachariasz@silevis.com>
2022-04-27 09:05:17 +0200

    Added create particle system with fog

--- src/game/scene/space/model/square/fog-of-war/fog-of-war.model.ts
+++ src/game/scene/space/model/square/fog-of-war/fog-of-war.model.ts
@@ -1,5 +1,6 @@
 import * as BABYLON from 'babylonjs';
 import {Model} from '../../model';
+import {SquareModel} from '../square.model';
 import {logic} from '../../../../../game';
 import earcut from 'earcut';
 
@@ -7,6 +8,10 @@ export class FogOfWarModel implements Model {
     public polygon: BABYLON.Mesh;
 
     private material: BABYLON.StandardMaterial;
+
+    public emitter: BABYLON.Mesh;
+
+    private particleSystem: BABYLON.ParticleSystem;
 
     constructor(private id: string,
                 private squarePoints: BABYLON.Vector3[]) {
@@ -18,8 +23,13 @@ export class FogOfWarModel implements Model {
         this.polygon.position.y = 1;
 
         this.material = new BABYLON.StandardMaterial('squarePolygonMaterial', scene);
-        this.material.alpha = 0.5;
+        this.material.alpha = 0;
         this.polygon.material = this.material;
+
+        this.emitter = polygonMeshBuilder.build(true);
+        this.emitter.setPositionWithLocalVector(new BABYLON.Vector3(this.mapPointsToVector2()[0].x, 2, this.mapPointsToVector2()[0].y));
+        this.emitter.material = new BABYLON.StandardMaterial('squareEmitterPolygonMaterial', scene);
+        this.emitter.material.alpha = 0;
 
         const actionManager: BABYLON.ActionManager = new BABYLON.ActionManager(scene);
         this.polygon.actionManager = actionManager;
@@ -29,6 +39,8 @@ export class FogOfWarModel implements Model {
                 logic().selectedUnitService.deselect();
             })
         );
+
+        this.createNewSystem(scene);
 /*
         actionManager.registerAction(
             new BABYLON.ExecuteCodeAction(BABYLON.ActionManager.OnPointerOverTrigger, () => {
@@ -53,5 +65,35 @@ export class FogOfWarModel implements Model {
 
     private mapPointsToVector2(): BABYLON.Vector2[] {
         return this.squarePoints.map((point: BABYLON.Vector3) => new BABYLON.Vector2(point.x, point.z));
+    }
+
+    private createNewSystem(scene: BABYLON.Scene): void {
+        const fogTexture = new BABYLON.Texture('resources/fog/fog.png', scene);
+
+        this.particleSystem = new BABYLON.ParticleSystem(('square_particles_' + this.id), 250, scene);
+        this.particleSystem.manualEmitCount = this.particleSystem.getCapacity();
+        this.particleSystem.minEmitBox = new BABYLON.Vector3(0, 0, 0); // Starting all from
+        this.particleSystem.maxEmitBox = new BABYLON.Vector3(SquareModel.SquareEdgeWidth, -2, SquareModel.SquareEdgeWidth); // To...
+        this.particleSystem.particleTexture = fogTexture.clone();
+        this.particleSystem.emitter = this.emitter;
+
+        this.particleSystem.color1 = new BABYLON.Color4(0.8, 0.8, 0.8, 0.1);
+        this.particleSystem.color2 = new BABYLON.Color4(.95, .95, .95, 0.15);
+        this.particleSystem.colorDead = new BABYLON.Color4(0.9, 0.9, 0.9, 0.1);
+        this.particleSystem.minSize = 3.5;
+        this.particleSystem.maxSize = 5.0;
+        this.particleSystem.minLifeTime = Number.MAX_SAFE_INTEGER;
+        this.particleSystem.emitRate = 50;
+        this.particleSystem.blendMode = BABYLON.ParticleSystem.BLENDMODE_STANDARD;
+        this.particleSystem.gravity = new BABYLON.Vector3(0, 0, 0);
+        this.particleSystem.direction1 = new BABYLON.Vector3(0, 0, 0);
+        this.particleSystem.direction2 = new BABYLON.Vector3(0, 0, 0);
+        this.particleSystem.minAngularSpeed = -2;
+        this.particleSystem.maxAngularSpeed = 2;
+        this.particleSystem.minEmitPower = .5;
+        this.particleSystem.maxEmitPower = 1;
+        this.particleSystem.updateSpeed = 0.005;
+
+        this.particleSystem.start();
     }
 }
