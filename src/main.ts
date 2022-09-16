9c37dd560802e3f2f89d30feac6d67d2de2f984c
tomasz.pasternak <tomasz.pasternak@silevis.com>
2022-09-16 11:46:44 +0200

    main to class

--- src/main.ts
+++ src/main.ts
@@ -1,71 +1,86 @@
 import {BrowserWindow, app, ipcMain} from 'electron';
 const electronLocalShortcut = require('electron-localshortcut');
 
-let mainWindow: Electron.BrowserWindow;
-let splashWindow: Electron.BrowserWindow;
+export class Main {
+    public mainWindow: Electron.BrowserWindow;
+    public splashWindow: Electron.BrowserWindow;
 
-function createWindow(): void {
-    splashWindow = new BrowserWindow({
-        height: 110,
-        width: 400,
-        alwaysOnTop: true,
-        transparent: true,
-        frame: false,
-        resizable: false
-    });
-    splashWindow.loadFile('splash-art.html');
-    splashWindow.center();
+    constructor() {
+        app.on('ready', () => {
+            this.createSplashWindow();
+            this.createMainWindow();
+        });
 
-    mainWindow = new BrowserWindow({
-        height: 1080,
-        width: 1920,
-        autoHideMenuBar: true,
-        webPreferences: {
-            nodeIntegration: true,
-            contextIsolation: false
-        },
-        show: false
-    });
+        app.on('window-all-closed', () => {
+            if (process.platform !== 'darwin') {
+                app.quit();
+            }
+        });
 
-    mainWindow.loadFile('index.html');
+        app.on('activate', () => {
+            if (this.mainWindow === null) {
+                this.createSplashWindow();
+                this.createMainWindow();
+            }
+        });
 
-    mainWindow.on('closed', () => {
-        mainWindow = null;
-    });
+        ipcMain.on('re-render', () => {
+            this.mainWindow.loadFile('index.html');
+        });
 
-    electronLocalShortcut.register(mainWindow, 'F5', () => {
-        mainWindow.reload();
-    });
+        ipcMain.on('game-root-scene-ready', () => {
+            this.splashWindow.close();
+            this.mainWindow.show();
+        });
+    }
 
-    electronLocalShortcut.register(mainWindow, 'F10', () => {
-        mainWindow.webContents.send('open-debug-layer');
-    });
+    private createMainWindow(): void {
+        this.mainWindow = new BrowserWindow({
+            height: 1080,
+            width: 1920,
+            autoHideMenuBar: true,
+            webPreferences: {
+                nodeIntegration: true,
+                contextIsolation: false
+            },
+            show: false
+        });
+        this.mainWindow.loadFile('index.html');
 
-    electronLocalShortcut.register(mainWindow, 'F12', () => {
-        mainWindow.webContents.openDevTools();
-    });
+        this.mainWindow.on('closed', () => {
+            this.mainWindow = null;
+        });
+
+        this.registerMainWindowEvents();
+    }
+
+    private registerMainWindowEvents(): void {
+        electronLocalShortcut.register(this.mainWindow, 'F5', () => {
+            this.mainWindow.reload();
+        });
+
+        electronLocalShortcut.register(this.mainWindow, 'F10', () => {
+            this.mainWindow.webContents.send('open-debug-layer');
+        });
+
+        electronLocalShortcut.register(this.mainWindow, 'F12', () => {
+            this.mainWindow.webContents.openDevTools();
+        });
+    }
+
+    private createSplashWindow(): void {
+        this.splashWindow = new BrowserWindow({
+            height: 110,
+            width: 400,
+            alwaysOnTop: true,
+            transparent: true,
+            frame: false,
+            resizable: false
+        });
+        this.splashWindow.loadFile('splash-art.html');
+        this.splashWindow.center();
+    }
 }
 
-app.on('ready', createWindow);
-
-app.on('window-all-closed', () => {
-    if (process.platform !== 'darwin') {
-        app.quit();
-    }
-});
-
-app.on('activate', () => {
-    if (mainWindow === null) {
-        createWindow();
-    }
-});
-
-ipcMain.on('re-render', () => {
-    mainWindow.loadFile('index.html');
-});
-
-ipcMain.on('game-root-scene-ready', () => {
-    splashWindow.close();
-    mainWindow.show();
-});
-
+const main = new Main();
+export default main;
